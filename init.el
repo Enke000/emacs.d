faec501fa290c61bc023d2fed95a05b4986a8bf1
Enke <enke000@gmail.com>
2020-06-15 21:27:02 +0800

    Update to lession 36. And turn on the auto complete brackets.

--- init.el
+++ init.el
@@ -19,21 +19,30 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Dropbox/org/inbox.org")))
  '(org-confirm-babel-evaluate nil)
  '(org-default-notes-file (concat org-directory "/notes.org"))
  '(org-directory "~/Dropbox/org")
  '(org-export-html-postamble nil)
  '(org-hide-leading-stars t)
+ '(org-image-actual-width nill)
  '(org-src-fontify-natively t)
  '(org-startup-folded (quote overview))
  '(org-startup-indented t)
- '(org-image-actual-width nill)
- )
+ '(package-selected-packages
+   (quote
+    (emmet-mode counsel-projectile projectile exec-path-from-shell pyenv-mode deft zenburn-theme which-key web-mode use-package try org-download org-bullets org-ac noflet jedi iedit hungry-delete htmlize flycheck expand-region elpy counsel cnfonts beacon ace-window))))
 
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+;; (put 'narrow-to-region 'disabled nil)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-(put 'narrow-to-region 'disabled nil)
--- myinit.el
+++ myinit.el
@@ -15,19 +15,46 @@
 ;;set command-key to meta
 (if (eq system-type 'darwin)
     (progn (setq mac-option-modifier 'super)
-           (setq mac-command-modifier 'meta))
-  (pass))
+           (setq mac-command-modifier 'meta)))
+;; turn off Simultaneous Editing
+(setq create-lockfiles nil)
+
+;; all backups goto ~/.backups instead in the current directory
+(setq backup-by-copying t
+      backup-directory-alist
+      '(("." . "~/.emacs.d/backup"))
+      delete-old-versions t
+      kept-new-versions 3
+      kept-old-versions 1
+      version-control t)
+
+;;auto-save
+(setq-default auto-save-timeout 15) ; 15秒无动作,自动保存
+(setq-default auto-save-interval 100) ; 100个字符间隔, 自动保存
+
+;;设置窗口位置为屏库左上角(0,0)
+(set-frame-position (selected-frame) 0 0)
+;;设置宽和高
+(set-frame-width (selected-frame) 170)
+(set-frame-height (selected-frame) 80)
+
+;; 最大化窗口
+;;(add-to-list 'default-frame-alist '(fullscreen . fullboth))
+;;(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+;;(add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
+
+(electric-pair-mode t)
 
 (setq fonts
       (cond ((eq system-type 'darwin)     '("Monaco"    "STHeiti"))
-	    ((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei"))
-	    ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei"))))
+            ((eq system-type 'gnu/linux)  '("Menlo"     "STHeiti"))
+            ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei"))))
 (set-face-attribute 'default nil :font
-		    (format "%s:pixelsize=%d" (car fonts) 14))
+                    (format "%s:pixelsize=%d" (car fonts) 14))
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
   (set-fontset-font (frame-parameter nil 'font) charset
-		    (font-spec :family (car (cdr fonts)))))
-;; Fix chinese font width and rescale
+                    (font-spec :family (car (cdr fonts)))))
+;;Fix chinese font width and rescale
 (setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Micro HeiMono" . 1.2) ("STHeiti". 1.2)))
 
 ;;(use-package org-bullets
@@ -35,34 +62,18 @@
 ;;  :config
 ;;  (add-hook 'org-mode-hook #'org-bullets-mode))
 
-(setenv "BROWSER" "safari")
-
-;; set org files directory
-(custom-set-variables
- '(org-directory "~/Dropbox/org")
- '(org-default-notes-file (concat org-directory "/inbox.org"))
- '(org-export-html-postamble nil)
- '(org-hide-leading-stars t)
- '(org-startup-folded (quote overview))
- '(org-startup-indented t)
- '(org-confirm-babel-evaluate nil)
- '(org-src-fontify-natively t)
- )
-
-;;set org-file-apps?
-(setq org-file-apps
-      (append '(
-                ("\\.pdf\\'" . "evince %s")
-                ("\\.x?html?\\'" . "/usr/bin/firefox %s")
-                ) org-file-apps ))
+;; set org-file-apps?
+;; (setq org-file-apps
+;;       (append '(
+;;                 ("\\.pdf\\'" . "evince %s")
+;;                 ("\\.x?html?\\'" . "/usr/bin/firefox %s")
+;;                 ) org-file-apps ))
 
 ;;key bind and some stuff
 (global-set-key "\C-ca" 'org-agenda)
 (setq org-agenda-start-on-weekday nil)
-(setq org-agenda-custom-commands
-      '(("c" "Simple agenda view"
-         ((agenda "")
-          (alltodo "")))))
+;;load agenda files
+(setq org-agenda-files (list "~/Dropbox/org/*.org"))
 
 ;;use org-ac auto-complete for org
 (use-package org-ac
@@ -71,11 +82,8 @@
           (require 'org-ac)
           (org-ac/config-default)
           ))
-;;set key bind to C-c c
+    ;;set key bind to C-c c
 (global-set-key (kbd "C-c c") 'org-capture)
-
-;;load agenda files
-(setq org-agenda-files (list "~/Dropbox/org/inbox.org"))
 
 ;;set org capture templates
 (setq org-capture-templates
@@ -87,18 +95,28 @@
          "* %u %? " :prepend t)
         ))
 
-;; use htmlize
-(use-package htmlize :ensure t)
 ;;images support
 (use-package org-download
   :ensure t
   :config
   (add-hook 'dired-mode-hook 'org-download-enable)
-  (setq-default org-download-heading-lvl nil)
-  (setq-default org-download-image-dir "~/Dropbox/org/notes/images"))
-;; set defauly img width in org.a
+  (setq org-download-heading-lvl nil)
+  (setq-default org-download-image-dir "~/Dropbox/org/images")
+  )
+;; set defauly img width in org
 (setq org-image-actual-width 500)
 
+
+;; (defun after-org-mode-loaded()
+;;   (setq org-download-heading-lvl nil)
+;;   (if buffer-file-name
+;;       (let ((org-img-base-folder "~/Dropbox/org/images")
+;;             (img-folder (format "%s/%s" org-img-base-folder (file-name-base buffer-file-name))))
+;;         (setq-local org-download-image-dir img-folder))
+;;     ))
+
+;; use htmlize
+(use-package htmlize :ensure t)
 (require 'ox-publish)
 (setq org-publish-project-alist
       '(
@@ -128,6 +146,25 @@
          )
         ("blog" :components ("blog-notes" "blog-static"))
         ))
+
+;;deft emacs
+(use-package deft
+  :ensure t
+  :bind ("C-c d" . deft)
+  :commands (deft)
+  :config (setq deft-directory "~/Dropbox/org/notes"
+                deft-extensions '("md" "org")
+                deft-recursive t
+                deft-extensions '("txt" "tex" "org" "md")
+                deft-file-naming-rules '((noslash . "-"))
+                deft-text-mode 'org-mode
+                deft-use-filter-string-for-filename t
+                deft-strip-summary-regexp
+                (concat "\\("
+                        "[\n\t]" ;; blank
+                        "\\|^#\\+[[:upper:]_]+:.*$" ;; org-mode metadata
+                        "\\|^#\\+[[:alnum:]_]+:.*$" ;; org-mode metadata
+                        "\\)")))
 
 (use-package try
   :ensure t)
@@ -198,16 +235,27 @@
   :init
   (global-flycheck-mode t))
 
+(use-package elpy
+  :ensure t
+  :config
+      (elpy-enable))
+
+;;(setq python-shell-interpreter "~/.pyenv/shims/python")
+
+(use-package pyenv-mode
+  :ensure t)
+
 (use-package jedi
   :ensure t
   :init
   (add-hook 'python-mode-hook 'jedi:setup)
-  (add-hook 'python-mode-hook 'jedi:ac-setup))
+      (add-hook 'python-mode-hook 'jedi:ac-setup))
 
-(use-package elpy
+(use-package exec-path-from-shell
   :ensure t
-  :config
-  (elpy-enable))
+  :init
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
 
 (use-package yasnippet
   :ensure t
@@ -258,3 +306,50 @@
     (if (> numfrs 1)
         ad-do-it
       (do-applescript "tell application \"System Events\" to tell process \"Emacs\" to set visible to false"))))
+
+(defun now ()
+  "Insert string for the current time formatted like '2:34 PM' or 1507121460"
+  (interactive)                 ; permit invocation in minibuffer
+  ;;(insert (format-time-string "%D %-I:%M %p")))
+  (insert (format-time-string "%-I:%M %p")))
+  ;;(insert (format-time-string "%02y%02m%02d%02H%02M%02S")))
+  ;;(insert (format-time-string "%02y%02m%02d%02H%02M")))
+
+(global-set-key "\C-xt" 'now)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(setq ibuffer-saved-filter-groups
+      (quote (("default"
+               ("dired" (mode . dired-mode))
+               ("org" (name . "^.*org$"))
+
+               ("web" (or (mode . web-mode) (mode . js2-mode)))
+               ("shell" (or (mode . eshell-mode) (mode . shell-mode)))
+               ("mu4e" (name . "\*mu4e\*"))
+               ("programming" (or
+                               (mode . python-mode)
+                               (mode . c++-mode)))
+               ("emacs" (or
+                         (name . "^\\*scratch\\*$")
+                         (name . "^\\*Messages\\*$")))
+               ))))
+(add-hook 'ibuffer-mode-hook
+          (lambda ()
+            (ibuffer-auto-mode 1)
+            (ibuffer-switch-to-saved-filter-groups "default")))
+
+;; don't show these
+;;(add-to-list 'ibuffer-never-show-predicates "zowie")
+;; Don't show filter groups if there are no buffers in that group
+(setq ibuffer-show-empty-filter-groups nil)
+
+;; Don't ask for confirmation to delete marked buffers
+(setq ibuffer-expert t)
+
+(use-package emmet-mode
+  :ensure t
+  :config
+  (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+  (add-hook 'web-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+  (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+  )
